48784b83d9c58f47332a84304b02280cb1b032a3
hemanth421 <hemanthraju88598@gmail.com>
2018-03-19 20:53:44 +0530

    Add files via upload

---
+++ DTvisual.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+import numpy as np
+from sklearn import tree
+iris=load_iris()
+test_index=[0,50,100]
+#training data
+train_target=np.delete(iris.target,test_index)
+train_data=np.delete(iris.data,test_index,axis=0)
+
+#testing data
+test_target=iris.target[test_index]
+test_data=iris.data[test_index]
+
+#training
+clf=tree.DecisionTreeClassifier()
+clf.fit(train_data,train_target)
+
+print(test_target)
+print(clf.predict(test_data))
+
+print(test_data[1],test_target[1])
